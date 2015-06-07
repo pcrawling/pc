f3891c88090c56e26602d3c78be2c06cb7ba97f8
mishk0 <proverstka@gmail.com>
2015-06-07 07:36:27 -0400

    change port

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.get('*', function(req, res) {
     res.render('index', { auth: !!req.user });
 });
 
-app.listen(config.secret.port, function() {
-    logger.trace("Express server listening on port " + config.secret.port);
+app.listen(secret.port, function() {
+    logger.trace("Express server listening on port " + secret.port);
 });
 
